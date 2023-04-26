349c9d9f03422e8650b96509bf6580269068de72
Bachar <bachar.sabra@solidnames.fr>
2023-04-26 12:25:31 +0200

    Or done

--- __tests__/03_Or.js
+++ __tests__/03_Or.js
@@ -2,7 +2,7 @@ describe("Or", function() {
 
   it('pipe character ( | ) is a regex OR', function() {
     const thisPatternWorks = /^dog|cat$/;
-    const fixThisPattern   = /^___$/;
+    const fixThisPattern   = /^hearts|clubs$/;
     
     expect( 'dog' ).toMatch(thisPatternWorks);
     expect( 'cat' ).toMatch(thisPatternWorks);
